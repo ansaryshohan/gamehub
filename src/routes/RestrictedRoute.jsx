4e761a89c22c33e4a2a30d1d63c485b91e28adf0
ansaryshohan <ansary2083@gmail.com>
2025-02-19 05:47:30 +0600

    restricted route is solved

--- src/routes/RestrictedRoute.jsx
+++ src/routes/RestrictedRoute.jsx
@@ -2,14 +2,24 @@ import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 const RestrictedRoute = ({children}) => {
-  const {user}= useAuthContext();
+  const {user,loading}= useAuthContext();
+  if (loading) {
+    return (
+      <div className="w-full h-[50vh] flex items-center justify-center">
+        <span className="loading loading-bars loading-xs"></span>
+        <span className="loading loading-bars loading-sm"></span>
+        <span className="loading loading-bars loading-md"></span>
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+    );
+  }
 
   if(!user?.email){
     return (
       <>{children}</>
     )
   }
-  return <Navigate to={window.location.pathname} replace />
+  return <Navigate to={"/"} replace />
   
 }
 
